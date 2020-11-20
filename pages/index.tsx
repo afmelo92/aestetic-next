e220499a1e2ed235fe7bb3279290d6d90b55119b
Andre Melo <andre.fabian.melo@gmail.com>
2020-11-19 23:57:17 -0300

    Eslint + prettier

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <h1>HELLO MADURO</h1>
+      <h3>Ola amiguinho tudo joia?</h3>
+    </>
+  );
+};
+
+export default Home;
